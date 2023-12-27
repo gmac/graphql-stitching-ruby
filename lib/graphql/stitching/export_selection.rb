a92d43c59245011404f3fe3935b758cced5fcbd4
Greg MacWilliam <gmacwill77@gmail.com>
2023-12-26 20:37:21 -0500

    better kwarg handling.

--- lib/graphql/stitching/export_selection.rb
+++ lib/graphql/stitching/export_selection.rb
@@ -20,8 +20,13 @@ module GraphQL
           "#{EXPORT_PREFIX}#{name}"
         end
 
+        # The argument assigning Field.alias changed from
+        # a generic `alias` hash key to a structured `field_alias` kwarg.
+        # See https://github.com/rmosolgo/graphql-ruby/pull/4718
+        FIELD_ALIAS_KWARG = !GraphQL::Language::Nodes::Field.new(field_alias: "a").alias.nil?
+
         def key_node(field_name)
-          if Util.graphql_version?(2, 2)
+          if FIELD_ALIAS_KWARG
             GraphQL::Language::Nodes::Field.new(field_alias: key(field_name), name: field_name)
           else
             GraphQL::Language::Nodes::Field.new(alias: key(field_name), name: field_name)

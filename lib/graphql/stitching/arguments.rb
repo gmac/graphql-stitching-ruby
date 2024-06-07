01d5c32eeb05eaaf2b11c03cd290231981ac2208
Greg MacWilliam <gmacwill77@gmail.com>
2024-06-06 23:45:41 -0400

    some tests.

--- lib/graphql/stitching/arguments.rb
+++ lib/graphql/stitching/arguments.rb
@@ -8,11 +8,10 @@ module GraphQL
         attr_reader :value
         attr_reader :type_name
 
-        def initialize(name:, value:, list: false, key_insert: false, type_name: nil)
+        def initialize(name:, value:, list: false, type_name: nil)
           @name = name
           @value = value
           @list = list
-          @key_insert = key_insert
           @type_name = type_name
         end
 
@@ -20,17 +19,20 @@ module GraphQL
           @list
         end
 
-        def key_insert?
-          @key_insert
+        def ==(other)
+          self.class == other.class &&
+            @name == other.name &&
+            @value == other.value &&
+            @type_name == other.type_name &&
+            @list == other.list?
         end
 
         def as_json
           {
-            type: "argument",
+            node: "argument",
             name: name,
             type_name: type_name,
             list: list? || nil,
-            key_insert: key_insert? || nil,
             value: value.as_json,
           }.tap(&:compact!)
         end
@@ -46,12 +48,16 @@ module GraphQL
         def as_json
           raise "unimplemented"
         end
+
+        def ==(other)
+          self.class == other.class && value == other.value
+        end
       end
 
       class ObjectValue < BaseValue
         def as_json
           {
-            type: "object",
+            node: "object",
             value: @value.map(&:as_json),
           }
         end
@@ -60,7 +66,7 @@ module GraphQL
       class KeyValue < BaseValue
         def as_json
           {
-            type: "key",
+            node: "key",
             value: @value,
           }
         end
@@ -69,7 +75,7 @@ module GraphQL
       class LiteralValue < BaseValue
         def as_json
           {
-            type: "literal",
+            node: "literal",
             value: @value,
           }
         end
@@ -86,17 +92,33 @@ module GraphQL
             .selections.first
             .arguments
 
-          args = ast.map do |node|
-            unless argument_def = argument_defs_by_name[node.name]
-              raise "Input `#{node.name}` is not a valid field argument."
-            end
-            build_argument(node, argument_def)
-          end
-
-          ObjectValue.new(args)
+          build_argument_set(ast, argument_defs_by_name)
         end
 
         private
+
+        def build_argument_set(nodes, argument_defs_by_name)
+          args = nodes.map do |n|
+            argument_def = if argument_defs_by_name
+              unless d = argument_defs_by_name[n.name]
+                raise "Input `#{n.name}` is not a valid argument."
+              end
+              d
+            end
+
+            build_argument(n, argument_def)
+          end
+
+          if argument_defs_by_name
+            argument_defs_by_name.each_value do |argument_def|
+              if argument_def.type.non_null? && !nodes.find { _1.name == argument_def.name }
+                raise "missing argument `#{_1.name}`"
+              end
+            end
+          end
+
+          args
+        end
 
         def build_argument(node, argument_def)
           value = if node.value.is_a?(GraphQL::Language::Nodes::InputObject)
@@ -110,7 +132,7 @@ module GraphQL
           Argument.new(
             name: node.name,
             value: value,
-            list: argument_def ? argument_def.type.list? : nil,
+            list: argument_def ? argument_def.type.list? : false,
             type_name: argument_def ? argument_def.type.unwrap.graphql_name : nil,
           )
         end
@@ -118,27 +140,15 @@ module GraphQL
         def build_object_value(node, object_def)
           if object_def
             if !object_def.kind.input_object? && !object_def.kind.scalar?
-              raise "Objects can only be built into input object and scalar positions"
+              raise "Objects can only be built into input object and scalar positions."
             elsif object_def.kind.scalar? && GraphQL::Schema::BUILT_IN_TYPES[object_def.graphql_name]
-              raise "Objects can only be built into custom scalar types"
+              raise "Objects can only be built into custom scalar types."
             elsif object_def.kind.scalar?
               object_def = nil
             end
           end
 
-          args = node.arguments.map do |n|
-            argument_def = if object_def
-              unless d = object_def.arguments[n.name]
-                raise "Input `#{n.name}` is not a valid argument " \
-                  "on input object `#{object_def.graphql_name}`."
-              end
-              d
-            end
-
-            build_argument(n, argument_def)
-          end
-
-          ObjectValue.new(args)
+          ObjectValue.new(build_argument_set(node.arguments, object_def&.arguments))
         end
       end
     end
--- test/graphql/stitching/arguments_test.rb
+++ test/graphql/stitching/arguments_test.rb
@@ -3,6 +3,8 @@
 require "test_helper"
 
 class GraphQL::Stitching::ArgumentsTest < Minitest::Test
+  include GraphQL::Stitching::Arguments
+
   class TestSchema < GraphQL::Schema
     class TestEnum < GraphQL::Schema::Enum
       value "YES"
@@ -30,30 +32,155 @@ class GraphQL::Stitching::ArgumentsTest < Minitest::Test
         f.argument(:keys, [ObjectKey])
       end
 
-      field :scalar_key, [Boolean], null: false do |f|
+      field :scalar_key, Boolean, null: false do |f|
         f.argument(:key, ScalarKey)
+      end
+
+      field :builtin_scalar_key, Boolean, null: false do |f|
+        f.argument(:key, String)
+      end
+
+      field :enum_key, Boolean, null: false do |f|
+        f.argument(:key, TestEnum)
       end
     end
 
     query Query
   end
 
-  def test_single_object_key
+  def test_builds_single_object_key_into_matching_input_object
     args_schema = TestSchema.query.get_field("objectKey").arguments
     template = "key: {namespace: $.namespace, key: $.key, ownerType: $.ref.__typename, ownerId: $.ref.id}"
-    pp GraphQL::Stitching::Arguments.parse(args_schema, template).as_json
+    expected = [Argument.new(
+      name: "key",
+      type_name: "ObjectKey",
+      list: false,
+      value: ObjectValue.new([
+        Argument.new(
+          name: "namespace",
+          type_name: "String",
+          value: KeyValue.new(["namespace"]),
+        ),
+        Argument.new(
+          name: "key",
+          type_name: "String",
+          value: KeyValue.new(["key"]),
+        ),
+        Argument.new(
+          name: "ownerType",
+          type_name: "String",
+          value: KeyValue.new(["ref", "__typename"]),
+        ),
+        Argument.new(
+          name: "ownerId",
+          type_name: "String",
+          value: KeyValue.new(["ref", "id"]),
+        ),
+      ]),
+    )]
+
+    assert_equal expected, GraphQL::Stitching::Arguments.parse(args_schema, template)
   end
 
-  def test_object_list_key
+  def test_builds_object_list_key_into_matching_input_object
     args_schema = TestSchema.query.get_field("objectListKey").arguments
-    template = "keys: {namespace: $.namespace, key: $.key, ownerType: $.ref.__typename, ownerId: $.ref.id}"
-    pp GraphQL::Stitching::Arguments.parse(args_schema, template).as_json
+    template = "keys: {namespace: $.namespace, key: $.key}"
+    expected = [Argument.new(
+      name: "keys",
+      type_name: "ObjectKey",
+      list: true,
+      value: ObjectValue.new([
+        Argument.new(
+          name: "namespace",
+          type_name: "String",
+          value: KeyValue.new(["namespace"]),
+        ),
+        Argument.new(
+          name: "key",
+          type_name: "String",
+          value: KeyValue.new(["key"]),
+        ),
+      ]),
+    )]
+
+    assert_equal expected, GraphQL::Stitching::Arguments.parse(args_schema, template)
   end
 
-  def test_scalar_key
+  def test_builds_object_into_custom_scalar
     args_schema = TestSchema.query.get_field("scalarKey").arguments
-    template = "key: {namespace: $.namespace, key: $.key, ownerType: $.ref.__typename, ownerId: $.ref.id}"
-    # binding.pry
-    pp GraphQL::Stitching::Arguments.parse(args_schema, template).as_json
+    template = "key: {namespace: $.namespace, key: $.key}"
+    expected = [Argument.new(
+      name: "key",
+      type_name: "ScalarKey",
+      list: false,
+      value: ObjectValue.new([
+        Argument.new(
+          name: "namespace",
+          type_name: nil,
+          value: KeyValue.new(["namespace"]),
+        ),
+        Argument.new(
+          name: "key",
+          type_name: nil,
+          value: KeyValue.new(["key"]),
+        ),
+      ]),
+    )]
+
+    assert_equal expected, GraphQL::Stitching::Arguments.parse(args_schema, template)
+  end
+
+  def test_errors_for_building_objects_into_builtin_scalars
+    args_schema = TestSchema.query.get_field("builtinScalarKey").arguments
+    template = "key: {namespace: $.namespace, key: $.key}"
+
+    assert_error "can only be built into custom scalar types" do
+      GraphQL::Stitching::Arguments.parse(args_schema, template)
+    end
+  end
+
+  def test_errors_for_building_objects_into_non_object_non_scalars
+    args_schema = TestSchema.query.get_field("enumKey").arguments
+    template = "key: {namespace: $.namespace, key: $.key}"
+
+    assert_error "can only be built into input object and scalar positions" do
+      GraphQL::Stitching::Arguments.parse(args_schema, template)
+    end
+  end
+
+  def test_errors_for_building_invalid_root_keys
+    args_schema = TestSchema.query.get_field("objectKey").arguments
+    template = "key: {namespace: $.namespace, key: $.key}, invalid: true"
+
+    assert_error "`invalid` is not a valid argument" do
+      GraphQL::Stitching::Arguments.parse(args_schema, template)
+    end
+  end
+
+  def test_errors_for_building_invalid_object_keys
+    args_schema = TestSchema.query.get_field("objectKey").arguments
+    template = "key: {namespace: $.namespace, key: $.key, invalid: true}"
+
+    assert_error "`invalid` is not a valid argument" do
+      GraphQL::Stitching::Arguments.parse(args_schema, template)
+    end
+  end
+
+  def test_errors_for_omitting_a_required_root_argument
+    args_schema = TestSchema.query.get_field("objectKey").arguments
+    template = "other: true"
+
+    assert_error "missing argument `key`" do
+      GraphQL::Stitching::Arguments.parse(args_schema, template)
+    end
+  end
+
+  def test_errors_for_omitting_a_required_object_argument
+    args_schema = TestSchema.query.get_field("objectKey").arguments
+    template = "key: {namespace: $.namespace}"
+
+    assert_error "missing argument `key`" do
+      GraphQL::Stitching::Arguments.parse(args_schema, template)
+    end
   end
 end
